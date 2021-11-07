d9511e38f1c4c7d8d1b98c10cdd292086df8495a
barbosaoli <gustavobarbosa7162@gmail.com>
2021-11-07 18:21:54 -0300

    renderizaçao do tabuleiro

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,26 @@
 const beginGame = () => {
-  let amountCards = prompt(
+  let amountOfCards = prompt(
     "Digite a quantidade de cartas.\nNúmero par entre 4 e 14."
   );
 
-  while (amountCards % 2 !== 0 || amountCards < 4 || amountCards > 14) {
-    amountCards = parseInt(
+  while (amountOfCards % 2 !== 0 || amountOfCards < 4 || amountOfCards > 14) {
+    amountOfCards = parseInt(
       prompt(
         "Para de perder tempo e digita um número certo logo moço.\nNúmero par entre 4 e 14."
       )
     );
   }
-  spreadCards();
+
+  let board = document.querySelector(".board");
+  board.style.width = (amountOfCards * (117 + 34)) / 2 + "px";
+
+  spreadCards(amountOfCards);
 };
 
-const spreadCards = () => {
-  const img = [
+const spreadCards = (amountOfCards) => {
+  let sortedCards = [];
+
+  const imgArr = [
     "assets/parrots/bobrossparrot.gif",
     "assets/parrots/explodyparrot.gif",
     "assets/parrots/fiestaparrot.gif",
@@ -23,4 +29,28 @@ const spreadCards = () => {
     "assets/parrots/tripletsparrot.gif",
     "assets/parrots/unicornparrot.gif",
   ];
+
+  //seleciona os pares de parrots que irão ser utilizados
+  for (let i = 0; i < amountOfCards / 2; i++) {
+    let randomIndex = Math.floor(imgArr.length * Math.random());
+
+    sortedCards.push(imgArr[randomIndex]);
+    sortedCards.push(imgArr[randomIndex]);
+
+    imgArr.splice(randomIndex, 1); //remove os gifs já utilizados
+  }
+
+  while (sortedCards.length !== 0) {
+    let card = document.createElement("div");
+    let img = document.createElement("img");
+    let board = document.querySelector(".board");
+
+    let randomIndex = Math.floor(sortedCards.length * Math.random());
+
+    img.setAttribute("src", sortedCards[randomIndex]);
+    card.appendChild(img);
+
+    board.appendChild(card);
+    sortedCards.splice(randomIndex, 1);
+  }
 };
